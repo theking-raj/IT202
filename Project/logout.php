d3ecec65ea85ad4f51a0bad289a2b197b22f26c8
Raj Patel <rp766@afsconnect1.njit.edu>
2020-10-20 02:15:53 -0400

    updating logout.php

--- Project/logout.php
+++ Project/logout.php
@@ -6,6 +6,6 @@
 session_unset();
 // destroy the session
 session_destroy();
-echo "You're logged out (proof by dumping the session)<br>";
-echo "<pre>" . var_export($_SESSION, true) . "</pre>";
+echo "You're logged out <br>";
+
 ?>
