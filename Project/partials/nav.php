8cb1b75f7651684864c6afe080363d18eadc6547
Raj Patel <rp766@afsconnect1.njit.edu>
2020-11-12 02:23:15 -0500

    updating

--- Project/partials/nav.php
+++ Project/partials/nav.php
@@ -4,15 +4,22 @@
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
 <nav>
-<ul class="nav">
-    <li><a href="home.php">Home</a></li>
-    <?php if (!is_logged_in()): ?>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="register.php">Register</a></li>
-    <?php endif; ?>
-    <?php if (is_logged_in()): ?>
-        <li><a href="profile.php">Profile</a></li>
-        <li><a href="logout.php">Logout</a></li>
-    <?php endif; ?>
-</ul>
+    <ul class="nav">
+        <li><a href="home.php">Home</a></li>
+        <?php if (!is_logged_in()): ?>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+        <?php endif; ?>
+        <?php if (has_role("Admin")): ?>
+            <li><a href="test_create_products.php">Create Products</a></li>
+            <li><a href="test_list_products.php">View Products</a></li>
+            <li><a href="test_create_cart.php">Create Cart</a></li>
+            <li><a href="test_edit_cart.php">Edit Cart</a></li>
+            <li><a href="test_list_cart.php">View Cart</a></li>
+        <?php endif; ?>
+        <?php if (is_logged_in()): ?>
+            <li><a href="profile.php">Profile</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        <?php endif; ?>
+    </ul>
 </nav>
